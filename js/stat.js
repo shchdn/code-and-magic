616e435a6d86dc37fae3ea693bedefce5692ae38
shchdn <shchedrin.georgy@gmail.com>
2020-06-03 15:00:50 +0300

    Выполнил второе задание

--- js/stat.js
+++ js/stat.js
@@ -15,12 +15,32 @@ window.renderStatistics = function(ctx, names, times){
     var STATS_BAR_WIDTH = 40;
     var STATS_BAR_PADDING = STATS_BAR_WIDTH + 50;
     var STATS_BAR_PLAYER_COLOR = 'rgba(255, 0, 0, 1)';
-    var STATS_BAR_X = CLOUD_X - STATS_BAR_PADDING + 20;
-    console.log(names.length);
+    var STATS_BAR_X = CLOUD_X - STATS_BAR_PADDING + 20;    
+    var STATS_BAR_Y = CLOUD_HEIGHT - 30;
+    var getMaxBarHeight = function(){
+        var maxTime = 0;
+        var nextTime = 0;
+        for (var i = 0; i < times.length; i++){
+            nextTime = times[i].toFixed(0);
+            if (nextTime > maxTime) {
+                maxTime = nextTime;
+            }
+        }
+        return STATS_BAR_HEIGHT / maxTime;
+    }
     for (var i = 0; i < names.length; i++){
-        ctx.fillText( times[i].toFixed(0), STATS_BAR_X + STATS_BAR_PADDING, CLOUD_Y + 30);
-        ctx.fillRect(STATS_BAR_X + STATS_BAR_PADDING, CLOUD_Y + 210, STATS_BAR_WIDTH, -150);
+        var statsBarHeight = getMaxBarHeight() * times[i]; 
+        ctx.fillStyle = '#000';
+        ctx.fillText(times[i].toFixed(0), STATS_BAR_X + STATS_BAR_PADDING, STATS_BAR_Y - statsBarHeight - 10);
+        ctx.fillText(names[i], STATS_BAR_X + STATS_BAR_PADDING, CLOUD_HEIGHT - 10);
+        if (names[i] == 'Вы'){
+            ctx.fillStyle = 'red';
+        }
+        else{
+            var l = 50 - Math.round(Math.random() * 40);
+            ctx.fillStyle = 'hsl(254, 100%, ' + l.toString() + '%)';
+        }
+        ctx.fillRect(STATS_BAR_X + STATS_BAR_PADDING, STATS_BAR_Y, STATS_BAR_WIDTH, -statsBarHeight);
         STATS_BAR_X += STATS_BAR_PADDING;
-    };
-
+    }
 }
